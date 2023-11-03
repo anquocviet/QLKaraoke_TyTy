549999f65269151a49d125813148b62a6769511f
VIE <anq.viet203@gmail.com>
2023-11-03 18:05:47 +0700

    Add all model (entity)

---
+++ src/main/java/model/CT_KhuyenMai.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ *
+ * @author vie
+ */
+public class CT_KhuyenMai {
+
+    private String maKhuyenMai;
+    private String tenKhuyenMai;
+    private LocalDate ngayBatDau;
+    private LocalDate ngayKetThuc;
+    private int luotSuDungConLai;
+    private int chietKhau;
+
+    public CT_KhuyenMai() {
+    }
+
+    public CT_KhuyenMai(String maKhuyenMai, String tenKhuyenMai, LocalDate ngayBatDau, LocalDate ngayKetThuc, int luotSuDungConLai, int chietKhau) {
+        this.maKhuyenMai = maKhuyenMai;
+        this.tenKhuyenMai = tenKhuyenMai;
+        this.ngayBatDau = ngayBatDau;
+        this.ngayKetThuc = ngayKetThuc;
+        this.luotSuDungConLai = luotSuDungConLai;
+        this.chietKhau = chietKhau;
+    }
+
+    public CT_KhuyenMai(String maKhuyenMai) {
+        this.maKhuyenMai = maKhuyenMai;
+    }
+
+    public String getMaKhuyenMai() {
+        return maKhuyenMai;
+    }
+
+    public void setMaKhuyenMai(String maKhuyenMai) {
+        this.maKhuyenMai = maKhuyenMai;
+    }
+
+    public String getTenKhuyenMai() {
+        return tenKhuyenMai;
+    }
+
+    public void setTenKhuyenMai(String tenKhuyenMai) {
+        this.tenKhuyenMai = tenKhuyenMai;
+    }
+
+    public LocalDate getNgayBatDau() {
+        return ngayBatDau;
+    }
+
+    public void setNgayBatDau(LocalDate ngayBatDau) {
+        this.ngayBatDau = ngayBatDau;
+    }
+
+    public LocalDate getNgayKetThuc() {
+        return ngayKetThuc;
+    }
+
+    public void setNgayKetThuc(LocalDate ngayKetThuc) {
+        this.ngayKetThuc = ngayKetThuc;
+    }
+
+    public int getLuotSuDungConLai() {
+        return luotSuDungConLai;
+    }
+
+    public void setLuotSuDungConLai(int luotSuDungConLai) {
+        this.luotSuDungConLai = luotSuDungConLai;
+    }
+
+    public int getChietKhau() {
+        return chietKhau;
+    }
+
+    public void setChietKhau(int chietKhau) {
+        this.chietKhau = chietKhau;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.maKhuyenMai);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CT_KhuyenMai other = (CT_KhuyenMai) obj;
+        return Objects.equals(this.maKhuyenMai, other.maKhuyenMai);
+    }
+
+    @Override
+    public String toString() {
+        return "CT_KhuyenMai{" + "maKhuyenMai=" + maKhuyenMai + ", tenKhuyenMai=" + tenKhuyenMai + ", ngayBatDau=" + ngayBatDau + ", ngayKetThuc=" + ngayKetThuc + ", luotSuDungConLai=" + luotSuDungConLai + ", chietKhau=" + chietKhau + '}';
+    }
+
+}
