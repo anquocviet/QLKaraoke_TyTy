67b95d550c97b65e4bae9372b865d269edd467b9
Apes <21040431.thanh@student.iuh.edu.vn>
2023-11-11 09:29:11 +0700

    Thay lam dung ma sao k up dulieu tu database len table dc, up len trc toi ve lam tiep

--- src/main/java/controllers/GD_QLDichVuController.java
+++ src/main/java/controllers/GD_QLDichVuController.java
@@ -7,11 +7,16 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import model.DichVu;
 
 /**
@@ -21,6 +26,11 @@ import model.DichVu;
  */
 public class GD_QLDichVuController implements Initializable {
 
+    /**
+     *
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         col_maDichVu.setCellValueFactory(new PropertyValueFactory<>("maDichVu"));
@@ -31,21 +41,66 @@ public class GD_QLDichVuController implements Initializable {
 
         danhSach_DichVu = DichVu.getAllDichVu();
         tableView_DichVu.setItems(danhSach_DichVu);
+        
+        docDuLieuTuTable();
+        handleEventInTable();
+    }
+
+    public void handleEventInTable() {
+        tableView_DichVu.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                docDuLieuTuTable();
+            }
+            
+        });
+    tableView_DichVu.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.UP || event.getCode() == KeyCode.DOWN) {
+                    docDuLieuTuTable();
+                }
+            }
+            
+        });
     }
     
-   @FXML
-   private TableView<DichVu> tableView_DichVu;
-   @FXML
-   private TableColumn<DichVu, String> col_maDichVu;
-   @FXML
-   private TableColumn<DichVu, String> col_tenDichVu;
-   @FXML
-   private TableColumn<DichVu, Integer> col_soLuong;
-   @FXML
-   private TableColumn<DichVu, Long> col_donGia;
-   @FXML
-   private TableColumn<DichVu, String> col_donViTinh;
+    public void docDuLieuTuTable() {
+        DichVu dv = tableView_DichVu.getSelectionModel().getSelectedItem();
+        if (dv == null) {
+            return;
+        }
+        txtMaDichVu.setText(dv.getMaDichVu());
+        txtTenDichVu.setText(dv.getTenDichVu());
+        txtSoLuong.setText(String.valueOf(dv.getSoLuong()));
+        txtDonGia.setText(String.valueOf(dv.getDonGia()));
+        txtDonViTinh.setText(dv.getDonViTinh());
+    }
+    
+    @FXML
+    private TextField txtMaDichVu;        
+    @FXML
+    private TextField txtTenDichVu;
+    @FXML
+    private TextField txtSoLuong;
+    @FXML
+    private TextField txtDonGia;
+    @FXML
+    private TextField txtDonViTinh;
+    
+    @FXML
+    private TableView<DichVu> tableView_DichVu;
+    @FXML
+    private TableColumn<DichVu, String> col_maDichVu;
+    @FXML
+    private TableColumn<DichVu, String> col_tenDichVu;
+    @FXML
+    private TableColumn<DichVu, Integer> col_soLuong;
+    @FXML
+    private TableColumn<DichVu, Long> col_donGia;
+    @FXML
+    private TableColumn<DichVu, String> col_donViTinh;
    
-   ObservableList<DichVu> danhSach_DichVu;
+    ObservableList<DichVu> danhSach_DichVu;
 
 }
