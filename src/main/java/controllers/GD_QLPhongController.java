083cfd3f33dda98ac097cacb9ac25518688b9984
LAPTOP-07CV9LDS\Admin <nguyenducthang985@gmail.com>
2023-11-12 16:08:40 +0700

    merge code

--- src/main/java/controllers/GD_QLPhongController.java
+++ src/main/java/controllers/GD_QLPhongController.java
@@ -4,23 +4,72 @@
  */
 package controllers;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import main.App;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Phong;
 
 /**
  * FXML Controller class
  *
- * @author fil
+ * @author thach
  */
 public class GD_QLPhongController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        sttCol.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(table.getItems().indexOf(param.getValue()) + 1));
+        maPhongCol.setCellValueFactory(new PropertyValueFactory<>("maPhong"));
+        sucChuaCol.setCellValueFactory(new PropertyValueFactory<>("sucChua"));
+        tinhTrangCol.setCellValueFactory(cellData -> {
+            int tinhTrang = cellData.getValue().getTinhTrang();
+            String tinhTrangString;
+            if (tinhTrang == 0) {
+                tinhTrangString = "PHÒNG TRỐNG";
+            } else if (tinhTrang == 1) {
+                tinhTrangString = "ĐÃ THUÊ";
+            } else {
+                tinhTrangString = "ĐÃ ĐẶT";
+            }
+            return new ReadOnlyStringWrapper(tinhTrangString);
+        });
+
+        giaPhongCol.setCellValueFactory(cellData -> {
+            long giaPhong = cellData.getValue().getGiaPhong();
+            return new ReadOnlyStringWrapper(giaPhong + " K/H");
+        });
+        loaiPhongCol.setCellValueFactory(cellData -> {
+            int loaiPhong = cellData.getValue().getLoaiPhong();
+            String loaiPhongString;
+            if (loaiPhong == 1) {
+                loaiPhongString = "VIP";
+            } else {
+                loaiPhongString = "THƯỜNG";
+            }
+            return new ReadOnlyStringWrapper(loaiPhongString);
+        });
+        table.setItems(Phong.layTatCaPhong());
     }
+
+    @FXML
+    private TableView<Phong> table;
+    @FXML
+    private TableColumn<String, Integer> sttCol;
+    @FXML
+    private TableColumn<Phong, String> maPhongCol;
+    @FXML
+    private TableColumn<Phong, Integer> sucChuaCol;
+    @FXML
+    private TableColumn<Phong, String> tinhTrangCol;
+    @FXML
+    private TableColumn<Phong, String> loaiPhongCol;
+    @FXML
+    private TableColumn<Phong, String> giaPhongCol;
 
 }
